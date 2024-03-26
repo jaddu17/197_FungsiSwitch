fae94748902fe9e2cdb94c895717bec7e53415c7
jaddu17 <shafajaddu@gmail.com>
2024-03-26 10:15:17 +0700

    Menghitung luas persegi panjang

--- Parameter/Parameter.cpp
+++ Parameter/Parameter.cpp
@@ -11,4 +11,20 @@ void prosedurLuas(int p, int l)
 int fungsiLuas(int p, int l)
 {
 	return p * l;
+} 
+
+int main()
+{
+	int panjang, lebar;
+	cout << "Masukkan panjang: ";
+	cin >> panjang;
+	cout << "Masukkan lebar: ";
+	cin >> lebar;
+
+	prosedurLuas(panjang, lebar);
+	cout << "Luas persegi panjang dengan prosedur: " << luas << endl;
+
+	cout << "Luas persegi panjang dengan fungsi: " << luas << endl;
+
+	return 0;
 }
